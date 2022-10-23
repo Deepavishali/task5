e972a3c9ac71b16e9468e0646cf191bb30a38917
Deepavishali <115686314+Deepavishali@users.noreply.github.com>
2022-10-23 20:28:35 +0530

    Update anon&iife.js

--- anon&iife.js
+++ anon&iife.js
@@ -25,128 +25,128 @@ console.log(`oddnumbers : [${oddnumber(array)}]`)
 
 //Second question
 //Anonymous function
-// function (str) {
-//     str = str.toLowerCase().split(' ');
-//     for (var i = 0; i < str.length; i++) {
-//       str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-//     } 
-//     return str.join(' ');
-//   }
+function (str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    } 
+    return str.join(' ');
+  }
 
 //IIFE function
-// (function(str){
-//     str = str.toLowerCase().split(' ');
-//      for(var i = 0; i < str.length; i++) {
-//        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-//        console.log(str.join(' '));
-//     } 
-//     })("MUDRA IS MY NAME");
+(function(str){
+    str = str.toLowerCase().split(' ');
+     for(var i = 0; i < str.length; i++) {
+       str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+       console.log(str.join(' '));
+    } 
+    })("MUDRA IS MY NAME");
 
 //Third question
 //Anonymous function
-// function(array){
-//     var sum = 0;
-//     for(var i = 0 ; i< array.length ; i++){
-//        sum = sum + array[i];
-//      }
-//      return sum;
-//   }
+function(array){
+    var sum = 0;
+    for(var i = 0 ; i< array.length ; i++){
+       sum = sum + array[i];
+     }
+     return sum;
+  }
 
 //IIFE function
-// (function(array){
-//     var sum = 0;
-//          for(var i = 0 ; i< array.length ; i++){
-//             sum = sum + array[i];
-//           }
-//           console.log(sum);
-//     })([1,2,3,4])
+(function(array){
+    var sum = 0;
+         for(var i = 0 ; i< array.length ; i++){
+            sum = sum + array[i];
+          }
+          console.log(sum);
+    })([1,2,3,4])
 
 //Fourth question
 //Anonymous function
-// function(numArray){
-//     numArray = numArray.filter((number) => {
-//       for (var i = 2; i <= Math.sqrt(number); i++) {
-//         if (number % i === 0) return false;
-//       }
-//       return true;
-//     });
-//     console.log(numArray);
-// }
+function(numArray){
+    numArray = numArray.filter((number) => {
+      for (var i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) return false;
+      }
+      return true;
+    });
+    console.log(numArray);
+}
 
 //IIFE function
-// (function(numArray){
-//        numArray = numArray.filter((number) => {
-//          for (var i = 2; i <= Math.sqrt(number); i++) {
-//            if (number % i === 0) return false;
-//          }
-//          return true;
-//        });
-//        console.log(numArray);
-//    })([1,2,3,4])
+(function(numArray){
+       numArray = numArray.filter((number) => {
+         for (var i = 2; i <= Math.sqrt(number); i++) {
+           if (number % i === 0) return false;
+         }
+         return true;
+       });
+       console.log(numArray);
+   })([1,2,3,4])
 
 //fifth question
 //Anonymous function
-// function (arr, n)
-//     {
-//         // Traversing each element of the array
-//         // and check if it is palindrome or not
-//         for (let i = 0; i < n; i++)
-//         {
-//             let ans = isPalindrome(arr[i]);
-//             if (ans == false)
-//                 return false;
-//         }
-//         return true;
-//     }
+function (arr, n)
+    {
+        // Traversing each element of the array
+        // and check if it is palindrome or not
+        for (let i = 0; i < n; i++)
+        {
+            let ans = isPalindrome(arr[i]);
+            if (ans == false)
+                return false;
+        }
+        return true;
+    }
 
 //IIFE function
-// (function(arr, n)
-//     {
-//     // Traversing each element of the array
-//     // and check if it is palindrome or not
-//     for (let i = 0; i < n; i++)
-//     {
-//     let ans = isPalindrome(arr[i]);
-//     if (ans == false)
-//     return false;
-//     }
-//     return true;
-//     })([1,2,3] , 3)
+(function(arr, n)
+    {
+    // Traversing each element of the array
+    // and check if it is palindrome or not
+    for (let i = 0; i < n; i++)
+    {
+    let ans = isPalindrome(arr[i]);
+    if (ans == false)
+    return false;
+    }
+    return true;
+    })([1,2,3] , 3)
 
 //Seventh question
 //Anonymous function
-// function(array){
-//     let dup = [...new Set(array)];
-//     console.log(dup);
-//   }
+function(array){
+    let dup = [...new Set(array)];
+    console.log(dup);
+  }
 
 //IIFE function
-// (function(array){
-//     let dup = [...new Set(array)];
-//     console.log(dup);
-//    })([1,1,2,3,4])
+(function(array){
+    let dup = [...new Set(array)];
+    console.log(dup);
+   })([1,1,2,3,4])
 
 //Eight question
 //Anonymous function
-// function(array , k){
-//     k = k % a.length;
-//       if(k < 0){
-//         k += a.length;
-//       }
+function(array , k){
+    k = k % a.length;
+      if(k < 0){
+        k += a.length;
+      }
 
-//       reverse(a, 0, a.length - k - 1);
-//       reverse(a, a.length - k, a.length - 1);
-//       reverse(a, 0, a.length - 1);
-//     }
+      reverse(a, 0, a.length - k - 1);
+      reverse(a, a.length - k, a.length - 1);
+      reverse(a, 0, a.length - 1);
+    }
 
 //IIFE function
-// (function(array , k){
-//     k = k % a.length;
-//       if(k < 0){
-//         k += a.length;
-//       }
+(function(array , k){
+    k = k % a.length;
+      if(k < 0){
+        k += a.length;
+      }
 
-//       reverse(a, 0, a.length - k - 1);
-//       reverse(a, a.length - k, a.length - 1);
-//       reverse(a, 0, a.length - 1);
-//     })([1,2,3,4] , 2)
+      reverse(a, 0, a.length - k - 1);
+      reverse(a, a.length - k, a.length - 1);
+      reverse(a, 0, a.length - 1);
+    })([1,2,3,4] , 2)
